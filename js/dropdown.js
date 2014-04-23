34574c99e9dea8427053a321f5de42d5851bd7af
El77 <elliott.aykroyd@gmail.com>
2014-04-23 20:29:21 +0100

    To do list added. Dropdown js simplified. Dropdown HTML cleaned up.

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,104 +1,34 @@
-$(document).ready(function () {
-    $('#primary-nav .international').on('click', function (e) {
-        e.preventDefault();
-        $('#primary-nav .international').siblings().removeClass('active');
-        $('#primary-nav .international').toggleClass('active');
-        if ($('#primary-nav .international').hasClass('active')) {
-            if ($('#slide-down').hasClass('open')) {
-                $('#slide-down .drop.open').hide().removeClass('open');
-                $('.drop.international').show().addClass('open')
-            } else {
-                $('#slide-down .drop').hide();
-                $('.drop.international').show().addClass('open');
-                $('#slide-down').slideDown(150).addClass('open')
-            }
+$(document).ready(function(){
+
+    // Hide the content with JS so it's available in non-js browsers
+    $('.drop').hide();
+
+    // Show the correct dropdown menu when clicking through the
+    // primary navigation menu
+    $('#primary-nav li').on('click', function(e){
+
+        var $this = $(this);
+        var target = $this.find('a').attr('href');
+
+        if ( $this.hasClass('active') ) {
+
+            $('#slide-down').slideUp(300, function(){
+                $('.drop').hide();
+                $('#primary-nav li').removeClass('active');
+            });
+
         } else {
-            $('#slide-down').slideUp(150).removeClass('open')
+
+            $this.addClass('active').siblings().removeClass('active');
+
+            $('.drop').hide();
+            $(target).show();
+            $('#slide-down').slideDown(300);
+
         }
+
+        e.preventDefault();
+
     });
-    $('#primary-nav .politics').on('click', function (e) {
-        e.preventDefault();
-        $('#primary-nav .politics').siblings().removeClass('active');
-        $('#primary-nav .politics').toggleClass('active');
-        if ($('#primary-nav .politics').hasClass('active')) {
-            if ($('#slide-down').hasClass('open')) {
-                $('#slide-down .drop.open').hide().removeClass('open');
-                $('.drop.politics').show().addClass('open')
-            } else {
-                $('#slide-down .drop').hide();
-                $('.drop.politics').show().addClass('open');
-                $('#slide-down').slideDown(150).addClass('open')
-            }
-        } else {
-            $('#slide-down').slideUp(150).removeClass('open')
-        }
-    });
-    $('#primary-nav .business').on('click', function (e) {
-        e.preventDefault();
-        $('#primary-nav .business').siblings().removeClass('active');
-        $('#primary-nav .business').toggleClass('active');
-        if ($('#primary-nav .business').hasClass('active')) {
-            if ($('#slide-down').hasClass('open')) {
-                $('#slide-down .drop.open').hide().removeClass('open');
-                $('.drop.business').show().addClass('open')
-            } else {
-                $('#slide-down .drop').hide();
-                $('.drop.business').show().addClass('open');
-                $('#slide-down').slideDown(150).addClass('open')
-            }
-        } else {
-            $('#slide-down').slideUp(150).removeClass('open')
-        }
-    });
-    $('#primary-nav .technology').on('click', function (e) {
-        e.preventDefault();
-        $('#primary-nav .technology').siblings().removeClass('active');
-        $('#primary-nav .technology').toggleClass('active');
-        if ($('#primary-nav .technology').hasClass('active')) {
-            if ($('#slide-down').hasClass('open')) {
-                $('#slide-down .drop.open').hide().removeClass('open');
-                $('.drop.technology').show().addClass('open')
-            } else {
-                $('#slide-down .drop').hide();
-                $('.drop.technology').show().addClass('open');
-                $('#slide-down').slideDown(150).addClass('open')
-            }
-        } else {
-            $('#slide-down').slideUp(150).removeClass('open')
-        }
-    });
-    $('#primary-nav .culture').on('click', function (e) {
-        e.preventDefault();
-        $('#primary-nav .culture').siblings().removeClass('active');
-        $('#primary-nav .culture').toggleClass('active');
-        if ($('#primary-nav .culture').hasClass('active')) {
-            if ($('#slide-down').hasClass('open')) {
-                $('#slide-down .drop.open').hide().removeClass('open');
-                $('.drop.culture').show().addClass('open')
-            } else {
-                $('#slide-down .drop').hide();
-                $('.drop.culture').show().addClass('open');
-                $('#slide-down').slideDown(150).addClass('open')
-            }
-        } else {
-            $('#slide-down').slideUp(150).removeClass('open')
-        }
-    });
-    $('#primary-nav .blogs').on('click', function (e) {
-        e.preventDefault();
-        $('#primary-nav .blogs').siblings().removeClass('active');
-        $('#primary-nav .blogs').toggleClass('active');
-        if ($('#primary-nav .blogs').hasClass('active')) {
-            if ($('#slide-down').hasClass('open')) {
-                $('#slide-down .drop.open').hide().removeClass('open');
-                $('.drop.blogs').show().addClass('open')
-            } else {
-                $('#slide-down .drop').hide();
-                $('.drop.blogs').show().addClass('open');
-                $('#slide-down').slideDown(150).addClass('open')
-            }
-        } else {
-            $('#slide-down').slideUp(150).removeClass('open')
-        }
-    })
+
 });
